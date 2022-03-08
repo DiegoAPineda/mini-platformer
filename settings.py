527ef6ca4e62050e78d9c466bdfeca39d3309e05
DiegoAPineda <diegopineda1231@gmail.com>
2022-03-08 02:03:35 -0500

    Set up level for platformer

---
+++ settings.py
@@ -0,0 +1,17 @@
+level_map = [
+'                                       ',
+'                                       ',
+'                                       ',
+'  XX     XXXX        XX   X            ',
+'  XX        X        XX             XXX',
+'  XXXX                               XX',
+'  XXXX  X       XX                   XX',
+'  XX       X   XXXX        XX       XXX',
+'           X   XXXX       XXX     XXXXX',
+'       XXXXX   XXXX    XXXXXXX    XXXXX',
+'XXXXXXXXXXXX   XXXX   XXXXXXXX    XXXXX']
+
+tile_size = 64
+
+screen_width = 1200
+screen_height = tile_size*len(level_map)
